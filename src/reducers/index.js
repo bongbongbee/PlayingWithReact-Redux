634b8138f6abb397b7dd1bf4b28f758c9178cbd5
Quek Yang Sheng Tyson <quektyson@gmail.com>
2017-05-26 16:35:16 +0800

    Add SelectionReducer for handling list item selection state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
+/* whatever key we provide in here will show up
+* in the global state object*/
 export default combineReducers({
-    libraries: LibraryReducer
+    libraries: LibraryReducer,
+    selectedLibraryId: SelectionReducer
 });
