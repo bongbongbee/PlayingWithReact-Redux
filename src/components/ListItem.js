2278e30647f471674c978a0bccb7857f0036cd79
Quek Yang Sheng Tyson <quektyson@gmail.com>
2017-05-27 21:23:46 +0800

    Wire up action and reducer for ListItem

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    renderDescription() {
+        const { library, selectedLibraryId } = this.props;
+        if (library.id === selectedLibraryId) {
+            return (
+                <Text>{library.description}</Text>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
-        console.log(this.props);
+        const { id, title } = this.props.library;
 
         return (
-            <CardSection>
-                <Text style={titleStyle}>{this.props.library.title}</Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -24,6 +42,8 @@ const styles = {
     }
 };
 
+const mapStateToProps = state => ({ selectedLibraryId: state.selectedLibraryId });
+
 /* connect(mapStateToProps, actions)
  * pass in null when you're not mapping any props */
-export default connect(null, actions)(ListItem);
+export default connect(mapStateToProps, actions)(ListItem);
--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,1 +1,11 @@
-export default () => null;
+/* if you ever return an undefined, redux will throw error
+ * for the initial state, return null. */
+export default (state = null, action) => {
+    switch (action.type) {
+        case 'select_library':
+            return action.payload;
+
+        default:
+            return state;
+    }
+};
